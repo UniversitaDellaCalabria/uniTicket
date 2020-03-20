d79c4057072ec633d780af0387a73bc32f4e25a1
Giuseppe <giuseppe.demarco@unical.it>
2020-03-20 12:53:34 +0100

    app.js takes random vc providers and little refactor

--- chat/settings.py
+++ chat/settings.py
@@ -1,2 +1,9 @@
 # time in seconds to maintain alive an active user in chat
 SECONDS_TO_KEEP_ALIVE = 120
+
+#
+VIDEOCONF_PROVIDERS = ['https://meet.jit.si/',
+                       'https://edumeet.geant.org/',
+                       'https://open.meet.garr.it/',
+                       # 'https://seeweb1.iorestoacasa.work/',
+                       ]
