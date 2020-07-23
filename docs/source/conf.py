042785d22cf4b36090573a2772541b9c392a4a1a
Francesco Filicetti <francesco.filicetti@unical.it>
2020-07-23 18:04:33 +0200

    main to master refactor in sphinx docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,4 +56,4 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-main_doc = 'index'
+master_doc = 'index'
