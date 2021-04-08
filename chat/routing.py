faaa0ffd956d7f7bdcaf6d84d1b26c573be2d2fd
Francesco Filicetti <francesco.filicetti@unical.it>
2021-04-08 10:16:11 +0200

    fix: chat ws path with asgi v3

--- chat/routing.py
+++ chat/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+(?:\-\w+)*)/$', consumers.ChatConsumer),
+    re_path(r'ws/chat/(?P<room_name>\w+(?:\-\w+)*)/$', consumers.ChatConsumer.as_asgi()),
 ]
 
