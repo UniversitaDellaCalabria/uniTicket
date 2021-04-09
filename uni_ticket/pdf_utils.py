60f1632ac9fbd5ba4d669a4f583e70a65440667a
Francesco Filicetti <francesco.filicetti@unical.it>
2021-04-09 11:29:53 +0200

    fix: pdf view on localhost and ASGI

--- uni_ticket/pdf_utils.py
+++ uni_ticket/pdf_utils.py
@@ -6,24 +6,24 @@ from django.http import HttpResponse
 
 from weasyprint import HTML
 
+
 def response_as_pdf(response, pdf_fname):
     html_page = response.content.decode('utf-8')
-    protocol = 'http://' if settings.DEBUG else 'https://'
-    hostname = 'localhost:8000' if settings.DEBUG else settings.HOSTNAME
 
-    production_static_url = protocol + hostname + settings.STATIC_URL
-    html_page_rewritten = re.sub(settings.STATIC_URL,
-                                 production_static_url,
-                                 html_page)
+    # if ASGI
+    # asgiref lock static files URLs calling
+    # if channels runs on same project server
+    if not settings.DEBUG or not hasattr(settings, 'ASGI_APPLICATION'):
+        protocol = 'http://' if settings.DEBUG else 'https://'
+        production_static_url = protocol + settings.HOSTNAME + settings.STATIC_URL
+        # set full path to static files
+        html_page = re.sub(settings.STATIC_URL,
+                           production_static_url,
+                           html_page)
+    html = HTML(string=html_page)
 
-    # production_media_url = protocol + hostname + settings.MEDIA_URL
-    # html_page_rewritten = re.sub(settings.MEDIA_URL,
-                                 # production_media_url,
-                                 # html_page_rewritten)
-
-    html = HTML(string=html_page_rewritten)
     pdf_path = settings.TMP_DIR + os.path.sep + pdf_fname
-    html.write_pdf(target=pdf_path);
+    html.write_pdf(target=pdf_path)
 
     f = open(pdf_path, 'rb')
     response = HttpResponse(f.read(), content_type='application/pdf')
