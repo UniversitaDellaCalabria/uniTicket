8e50adaa1679bdf52901bb0314ae849bc9cd0428
Francesco Filicetti <francesco.filicetti@unical.it>
2023-06-23 09:44:54 +0200

    fix: template data missing

--- uniticket/uni_ticket/views/manager.py
+++ uniticket/uni_ticket/views/manager.py
@@ -65,7 +65,6 @@ def dashboard(request, structure_slug, structure):
     my_opened = assignments.filter(ticket__assigned_date__isnull=False, ticket__is_closed=False, taken_by=request.user).values('ticket__code').annotate(total=Count('ticket__code')).count()
     om = OrganizationalStructureOffice
     offices = om.objects.filter(organizational_structure=structure)\
-                        .values('is_active','is_default','name','description','slug')\
                         .prefetch_related('organizationalstructureofficeemployee_set')\
                         .prefetch_related('ticketassignment_set')\
                         .prefetch_related('ticketcategory_set')
