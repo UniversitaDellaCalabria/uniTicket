64e816928ba4acd6b8771c4b1405c8f44d0bc114
Francesco Filicetti <francesco.filicetti@unical.it>
2024-10-16 09:43:15 +0200

    fix: accounts settings

--- uniticket/accounts/settings.py
+++ uniticket/accounts/settings.py
@@ -8,4 +8,4 @@ REQUIRED_FIELDS = getattr(settings, 'REQUIRED_FIELDS', EDITABLE_FIELDS)
 CHANGE_EMAIL_TOKEN_LIFE = getattr(settings, 'CHANGE_EMAIL_TOKEN_LIFE', 30)
 
 SAFE_URL_PATHS = getattr(settings, 'SAFE_URL_PATHS', [])
-SAFE_URL_APPS = getattr(settings, 'SAFE_URL_APPS', ['admin', 'api_rest'])
+SAFE_URL_APPS = getattr(settings, 'SAFE_URL_APPS', ['admin', 'accounts', 'api_rest'])
