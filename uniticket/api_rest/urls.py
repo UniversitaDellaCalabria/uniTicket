c0346fc56a3bf06940c655c4e0104cab1a8ac6e8
Francesco Filicetti <francesco.filicetti@unical.it>
2024-12-02 16:52:17 +0100

    fix: operator counters API

--- uniticket/api_rest/urls.py
+++ uniticket/api_rest/urls.py
@@ -23,10 +23,14 @@ urlpatterns = [
 
     re_path('^openapi/$',  get_schema_view(**{}), name='openapi-schema'),
     re_path('^openapi.json/$', get_schema_view(renderer_classes = [JSONOpenAPIRenderer], **{}), name='openapi-schema-json'),
-    path('api/<slug:structure_slug>/<slug:category_slug>/ticket/new/', user.TicketAPIView.as_view(), name='api-new-ticket'),
-    path('api/ticket/<str:ticket_uid>/', user.TicketAPIDetail.as_view(), name='api-view-ticket'),
+
+    # generic
     path('api/strutture/list/', generic.TicketAPIStruttureList.as_view(), name='api-strutture-list'),
     path('api/ticket/category/list/', generic.TicketAPITicketCategoryList.as_view(),name='api-ticket-category-list'),
+
+    # user
+    path('api/<slug:structure_slug>/<slug:category_slug>/ticket/new/', user.TicketAPIView.as_view(), name='api-new-ticket'),
+    path('api/ticket/<str:ticket_uid>/', user.TicketAPIDetail.as_view(), name='api-view-ticket'),
     path('api/ticket/user/list/', user.TicketAPIListCreated.as_view(), name='api-ticket-user-list'),
     path('api/ticket/close/<str:ticket_id>/', user.TicketAPIClose.as_view(), name='api-ticket-close'),
 
@@ -37,4 +41,8 @@ urlpatterns = [
     path(f'api/{slugify(MANAGEMENT_URL_PREFIX["manager"])}/<slug:structure_slug>/tickets/messages/count/', manager.TicketAPIMessagesCounter.as_view(), name='api-manager-tickets-messages-count'),
 
     # operator
-    path(f'api/{slugify(MANAGEMENT_URL_PREFIX["operator"])}/<slug:structure_slug>/tickets/count/', operator.TicketAPICounter.as_view(), name='api-operator-tickets-count'),]
+    path(f'api/{slugify(MANAGEMENT_URL_PREFIX["operator"])}/<slug:structure_slug>/tickets/unassigned/count/', operator.TicketAPIUnassignedCounter.as_view(), name='api-operator-tickets-unassigned-count'),
+    path(f'api/{slugify(MANAGEMENT_URL_PREFIX["operator"])}/<slug:structure_slug>/tickets/open/count/', operator.TicketAPIOpenCounter.as_view(), name='api-operator-tickets-open-count'),
+    path(f'api/{slugify(MANAGEMENT_URL_PREFIX["operator"])}/<slug:structure_slug>/tickets/my-open/count/', operator.TicketAPIMyOpenCounter.as_view(), name='api-operator-tickets-my-open-count'),
+    path(f'api/{slugify(MANAGEMENT_URL_PREFIX["operator"])}/<slug:structure_slug>/tickets/messages/count/', operator.TicketAPIMessagesCounter.as_view(), name='api-operator-tickets-messages-count'),
+]
--- uniticket/api_rest/views/operator.py
+++ uniticket/api_rest/views/operator.py
@@ -24,41 +24,56 @@ from . generic import *
 logger = logging.getLogger(__name__)
 
 
-class TicketAPICounter(TicketAPIBaseView):
+class TicketAPIUnassignedCounter(TicketAPIBaseView):
     def get(self, request, structure_slug, *args, **kwargs):
         structure = get_object_or_404(OrganizationalStructure, slug=structure_slug)
         oe = user_is_operator(request.user, structure)
         if not oe: raise PermissionDenied
-
         unassigned_tickets = visible_tickets_to_user(user=request.user,
                                                      structure=structure,
                                                      office_employee=oe,
                                                      closed=False,
                                                      taken=False)
+        return Response({'count': unassigned_tickets.count()})
 
+
+class TicketAPIOpenCounter(TicketAPIBaseView):
+    def get(self, request, structure_slug, *args, **kwargs):
+        structure = get_object_or_404(OrganizationalStructure, slug=structure_slug)
+        oe = user_is_operator(request.user, structure)
+        if not oe: raise PermissionDenied
         open_tickets = visible_tickets_to_user(user=request.user,
                                                structure=structure,
                                                office_employee=oe,
                                                closed=False,
                                                taken=True)
+        return Response({'count': open_tickets.count()})
 
+
+class TicketAPIMyOpenCounter(TicketAPIBaseView):
+    def get(self, request, structure_slug, *args, **kwargs):
+        structure = get_object_or_404(OrganizationalStructure, slug=structure_slug)
+        oe = user_is_operator(request.user, structure)
+        if not oe: raise PermissionDenied
         my_open_tickets = visible_tickets_to_user(user=request.user,
                                                   structure=structure,
                                                   office_employee=oe,
                                                   closed=False,
                                                   taken=True,
                                                   taken_by=request.user)
+        return Response({'count': my_open_tickets.count()})
 
+
+class TicketAPIMessagesCounter(TicketAPIBaseView):
+    def get(self, request, structure_slug, *args, **kwargs):
+        structure = get_object_or_404(OrganizationalStructure, slug=structure_slug)
+        oe = user_is_operator(request.user, structure)
+        if not oe: raise PermissionDenied
         ticket_ids = visible_tickets_to_user(
             user=request.user,
             structure=structure,
             office_employee=oe,
             closed=False
         )
-
         messages = TicketReply.get_unread_messages_count(ticket_ids=ticket_ids)
-
-        return Response({'unassigned': len(unassigned_tickets),
-                         'open': len(open_tickets),
-                         'my_open': len(my_open_tickets),
-                         'new_messages': messages})
+        return Response({'count': messages})
